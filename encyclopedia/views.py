2f2494b6b678c441ca35ab1432c4f77b994dfb02
Marie-Sophie Trübenbach <ufsda@student.kit.edu>
2022-08-15 11:25:55 +0200

    Entry Page functionality works, but doesnt look pretty yet

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def title(request, title):
+    return render(request, "encyclopedia/title.html", {
+        "entry": util.get_entry(title),
+        "title": title
+    })
